f68c9e0bfe98ad9b4775f123564d4674f0284cf9
William Best <wbest@loanbright.com>
2017-10-30 11:02:01 -0600

    Completed the EF Core on ASP.NET Core with a New database walkthrough

---
+++ EFGetStarted.AspNetCore.NewDb/Models/BloggingContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
